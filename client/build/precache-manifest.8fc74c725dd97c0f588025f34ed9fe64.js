ecb83433deaef62de171ed7e2708cac1ffb98de8
Brian <rac3k@aol.com>
2018-10-16 18:38:20 -0400

    added files and folders

---
+++ client/build/precache-manifest.8fc74c725dd97c0f588025f34ed9fe64.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "83c45cd45d5147d966a4",
+    "url": "/static/js/main.83c45cd4.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "83c45cd45d5147d966a4",
+    "url": "/static/css/main.b1613cd3.chunk.css"
+  },
+  {
+    "revision": "71df6da30635194acbd6077bc59a409b",
+    "url": "/index.html"
+  }
+];
